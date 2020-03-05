a2a89ab967921132c360a3c6477bbe731c7e9542
Hector Rizo <hectormrizo@gmail.com>
2020-03-04 21:29:10 -0500

    Adding comment

--- Cell.py
+++ Cell.py
@@ -1,4 +1,11 @@
 
+"""
+I thought it would be a good idea to seperate the classes by file for better readability
+"""
+
+
+
+
 class Cell:
 #-----------------------------------------------------------------------------------
 #core atributes
