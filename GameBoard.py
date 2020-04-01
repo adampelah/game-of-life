ac1c016fc565a7526cc6395f5dae29fb8e952e7f
tripplerizz <hectormrizo@gmail.com>
2020-04-01 12:53:03 -0400

    deleted set up grid. realized it gets over written by fill grid. created population function in board class

--- GameBoard.py
+++ GameBoard.py
@@ -10,18 +10,44 @@ class Board:
     size = 100
     grid = np.zeros((size, size))
     cell_dict = {}
+    infectedCount = 0
+    population = 0
     
-    def __init__(self):
-        self.board = np.matrix('0 0 0; 0 0 0; 0 0 0')
-        self.store_y = 0
-        self.store_x = 0
+    def __init__(self, inSize): # creating parameter for user
+        
+        self.size = inSize
+        self.grid = np.zeros((inSize,inSize))
+        cell_dict = {}
 
-    def setUpGrid(self):
-        for x in range(0, 99):
-            for y in range(0, 99):
-                if (x, y) in self.cell_dict:
-                    self.grid[x][y] = self.cell_dict[x, y].infectionStatus  # Fill grid spot
+    def createPopulation (self, population, infected): # this function creates a population based on perameters and board
 
+        self.population = population
+        self.infectedCount = infected
+        randomCoord = [0,0]
+        randomCoord[0] = (rd.randint(0, self.size -1))  # location x
+        randomCoord[1] = (rd.randint(0, self.size -1))  # location y
+
+        for x in range(0, population):
+
+            infectionStatus = 1
+            if x < infected: # this condition creates number of ifected sells specified by perameters
+                infectionStatus = 2
+            
+            while (self.grid[randomCoord[0], randomCoord[1]] != 0):
+                randomCoord[0] = rd.randint(0, self.size - 1)  # location x
+                randomCoord[1] = rd.randint(0, self.size -1)  # location y
+
+            new_cell = Cell(rd.randint(1, 80),  # age
+                            infectionStatus,  # infection stat
+                            randomCoord[0],  # location x
+                            randomCoord[1],  # location y
+                            rd.randint(0, 255),  # time virus
+                            rd.randint(0, 255),  # time
+                            self.grid) 
+
+            self.cell_dict[new_cell.Row, new_cell.Column] = new_cell
+            self.grid[new_cell.Row, new_cell.Column] = new_cell.infectionStatus
+        
     def update_grid(self):
         for x in range(0, 99):
             for y in range(0, 99):
@@ -31,40 +57,6 @@ class Board:
                     popped_cell.move()  # call move on cell
                     self.cell_dict[popped_cell.Row, popped_cell.Column] = popped_cell
                     self.grid[popped_cell.Row][popped_cell.Column] = popped_cell.infectionStatus
-
-    def fill_dict(self):
-        infectedCount = 0
-        healthyCount = 0
-        randomCoord = []
-        randomCoord.append(rd.randint(0, 99))  # location x
-        randomCoord.append(rd.randint(0, 99))  # location y
-
-        for x in range(0, 200):
-
-            while (self.grid[randomCoord[0], randomCoord[1]] != 0):
-                randomCoord[0] = rd.randint(0, 99)  # location x
-                randomCoord[1] = rd.randint(0, 99)  # location y
-
-            infectionChance = rd.randint(1, 3)
-            if (infectionChance == 3):
-                infectedStatus = 2
-                infectedCount += 1
-            else:
-                healthyCount += 1
-
-            infectedStatus = 1
-            new_cell = Cell(rd.randint(1, 80),  # age
-                            infectedStatus,  # infection stat
-                            randomCoord[0],  # location x
-                            randomCoord[1],  # location y
-                            rd.randint(0, 255),  # time virus
-                            rd.randint(0, 255),  # time
-                            self.grid)
-
-            self.cell_dict[new_cell.Row, new_cell.Column] = new_cell
-            self.grid[new_cell.Row, new_cell.Column] = new_cell.infectionStatus
-
-
 
 
     def show(self):
--- GermTheory.py
+++ GermTheory.py
@@ -4,9 +4,9 @@ from GameBoard import Board
 sys.path.append("..")
 
 def main():
-    b1 = Board()
-    b1.fill_dict()
-    b1.setUpGrid()
+    b1 = Board(100)
+    b1.createPopulation(100, 1)
+    b1.update_grid()
     b1.show()
        
 main()
