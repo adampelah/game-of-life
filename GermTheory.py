b1eba2ef315c67e7e6d74c5e7774f1848fe876ed
tripplerizz <hectormrizo@gmail.com>
2020-04-03 20:48:27 -0400

    adding user input

--- GermTheory.py
+++ GermTheory.py
@@ -4,8 +4,10 @@ from GameBoard import Board
 sys.path.append("..")
 
 def main():
+
+
     b1 = Board(100)
-    b1.createPopulation(1000, 25)
+    b1.createPopulation( 1000 , input("how many infected members: "))
     b1.update_grid()
     b1.show()
        
