3f11b7411ec82d166c15d69987d075b341adcddc
akoelsch <47152391+akoelsch@users.noreply.github.com>
2020-04-24 16:49:55 -0400

    Update GermTheory.py

--- GermTheory.py
+++ GermTheory.py
@@ -6,8 +6,6 @@ sys.path.append("..")
 
 def main():
     b1 = Board(100)  # Create board
-    #b1.createPopulation(input("What state are you in? "))  # Create population
-    b1.update_grid()  # Update gridlo
     b1.menu()  # Show
 
 
